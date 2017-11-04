d8e2554e756ceb21553348a9ffffdd8ad7832533
Ponacko <ponacko@gmail.com>
2017-11-04 19:10:40 +0100

    Scrape web and display in console

---
+++ BeerRecommender/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace BeerRecommender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var web = new HtmlWeb();
+            var url = "https://www.ipivovary.cz/seznam-pivovaru";
+            var doc = web.Load(url);
+            var pivovary = doc.DocumentNode.SelectNodes("//div[@class='rowseznampivo']");
+            foreach (var pivovar in pivovary) {
+                var attributes = pivovar.SelectNodes(".//div[contains(@class,'seznampivocell')]");
+                Console.WriteLine($"Pivovar {attributes[0].InnerText} na adrese {attributes[1].InnerText} ma hodnotenie {attributes[4].InnerText}.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
