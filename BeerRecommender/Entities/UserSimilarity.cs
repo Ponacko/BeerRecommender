7b0455fab85a6da1a4f80dcb5967273cf05d3abb
Matúš Sedlák <sedlak87@gmail.com>
2017-11-11 12:50:53 +0100

    user similarity entity + treba naimplementovat SimilarityService

---
+++ BeerRecommender/Entities/UserSimilarity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerRecommender.Entities
+{
+    public class UserSimilarity
+    {
+        public User user1 { get; set; }
+        public User user2 { get; set; }
+        
+        public int Similarity { get; set; }
+    }
+}
