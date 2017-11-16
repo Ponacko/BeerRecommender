aa2cc6a77276e69d102bb4758f0530866bf9f268
Ponacko <ponacko@gmail.com>
2017-11-16 17:41:06 +0100

    Add calculating user bias

--- BL/Services/SimilarityService.cs
+++ BL/Services/SimilarityService.cs
@@ -13,12 +13,15 @@ namespace BL.Services
     public class SimilarityService
     {
         public static double CalculateSimilarity(User user1, User user2) {
+            var bias1 = CalculateBias(user1);
+            var bias2 = CalculateBias(user2);
             var ratings = user1.UserRatings
                 .Where(u => !u.IsPrediction)
                 .Join(user2.UserRatings
                     .Where(u => !u.IsPrediction), r1 => r1.Beer, r2 => r2.Beer, 
-                    (r1, r2) => new { Rating1 = r1.Rating, Rating2 = r2.Rating}).ToList();
-            return Correlation.Pearson(ratings.Select(r => (double)r.Rating1), ratings.Select(r => (double)r.Rating2));
+                    (r1, r2) => new { Rating1 = r1.Rating - bias1, Rating2 = r2.Rating - bias2}).ToList();
+            
+            return Correlation.Pearson(ratings.Select(r => r.Rating1), ratings.Select(r => r.Rating2));
         }
 
         public static int AddSimilarity(User user1, User user2) {
@@ -29,6 +32,10 @@ namespace BL.Services
             };
             var repository = new UserSimilarityRepository();
             return repository.Create(similarity);
+        }
+
+        public static double CalculateBias(User user) {
+            return user.UserRatings.Average(ur => ur.Rating);
         }
     }
 }
