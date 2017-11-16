14507f6f2cece9407ba12906bf4b9001c26ebe36
Ponacko <ponacko@gmail.com>
2017-11-16 18:19:58 +0100

    Add general recommendations and updating average rating

---
+++ BL/Services/RecommendationService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using BeerRecommender;
+using BeerRecommender.Repositories;
+
+namespace BL.Services
+{
+    public class RecommendationService
+    {
+        public IEnumerable<Beer> GetMostPopularBeers(int numberOfBeers) {
+            var repository = new BeerRepository();
+            var beers = repository.RetrieveAll();
+            beers.ForEach(UpdateAverageRating);
+            return beers.OrderByDescending(b => b.AverageRating).Take(numberOfBeers);
+        }
+
+        public void UpdateAverageRating(Beer beer) {
+            var repository = new BeerRepository();
+            beer.AverageRating = beer.UserRatings.Average(ur => ur.Rating);
+            repository.Update(beer);
+        }
+
+        // TODO: personalized recommendations based on most similar users
+    }
+}
