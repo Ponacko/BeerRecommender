97ccdea05bd3ec8a2d0ba41892f4baf99f26a9fe
Ponacko <ponacko@gmail.com>
2017-11-26 19:12:43 +0100

    Add new repositories

---
+++ BeerRecommender/Repositories/RegionRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeerRecommender.Entities;
+
+namespace BeerRecommender.Repositories
+{
+    public class RegionRepository : Repository<Region>
+    {
+        public RegionRepository() {
+            context = new AppDbContext();
+            entities = context.Regions;
+        }
+    }
+}
