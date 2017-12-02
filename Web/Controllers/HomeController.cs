14e5302e8d75318764d64292ae8fb72f45c0354d
Ponacko <ponacko@gmail.com>
2017-12-02 12:36:10 +0100

    Display beer images on front end

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -3,30 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BeerRecommender;
 using BL.Services;
 
-namespace Web.Controllers
-{
-    public class HomeController : Controller
-    {
+namespace Web.Controllers {
+    public class HomeController : Controller {
         public ActionResult Index() {
-            var beers = BeerService.GetAllBeers();
-            ViewBag.Beers = beers;
+            var beers = BeerService.GetAllBeers().Take(30);
+            var enumerable = beers as Beer[] ?? beers.ToArray();
+            ViewBag.Beers = enumerable.ToList();
+            ViewBag.Count = enumerable.Count();
             return View();
         }
 
-        public ActionResult About()
-        {
+        public ActionResult About() {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
+        public ActionResult Contact() {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
     }
-}
+} ;
